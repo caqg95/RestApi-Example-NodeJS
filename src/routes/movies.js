8ad8b531d7037df15f244549b17aeb9f9e556331
caqg95 <cquinterogrande@yahoo.es>
2019-05-22 09:52:31 -0600

    Underscored

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -9,8 +9,21 @@ router.get('/',(req,res)=>{
 });
 
 router.post('/',(req,res)=>{
-    console.log(req.body);
-    res.send('received');
+    const {title,director,year,rating}=req.body;
+    if(title && director && year && rating){
+        const id= movies.length + 1;
+        const newMovie={id,...req.body};
+        movies.push(newMovie);
+        res.json(movies);
+    }
+    else{
+          res.status(500).json({error:"There was an error"});
+    }
+  
+});
+router.delete('/:id',(req,res)=>{
+    const { }=req.params;
+    res.send('Deleted');
 });
 
 module.exports=router;
